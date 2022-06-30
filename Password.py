9ab61462f89af9487a78f07458c9ef5837cc2479
Dibyani Mohanty <107949982+cheryleen@users.noreply.github.com>
2022-06-30 15:45:23 +0530

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,23 +1,11 @@
-# Check = "^[a-z][A-Z][0-9][@&%]"
 import re
-PASSWORD=input("Input your password")
-def check(x):
-    while True:
-        if (len(x)<5 or len(x)>10):
-            break
-        elif not re.search("[0-9]",x):
-            break
-        elif not re.search("[a-z]",x):
-            break
-        elif not re.search("[@&%]",x):
-            break
-        elif not re.search("[A-Z]",x):
-            break
-        elif re.search("\s",x):
-            break
-        else:
-            print("Valid Password")
-            break
-    if False:
-        print("Not a valid Password")
-print(check(PASSWORD))
+
+Password = input("Enter the password here: ")
+pattern = "\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@%&])[A-Za-z\d@%&]{5,10}$"
+
+x = re.findall(pattern, Password)
+
+if x:
+   print("Valid")
+else:
+   print("Invalid")
