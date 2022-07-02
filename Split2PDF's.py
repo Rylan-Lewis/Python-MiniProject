fe2da04402bad5241779bde865e85124c1a20bf2
mustafa-motiwala07 <102900301+mustafa-motiwala07@users.noreply.github.com>
2022-07-03 01:23:22 +0530

    Update Split2PDF's.py

--- Split2PDF's.py
+++ Split2PDF's.py
@@ -1,5 +1,5 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
-input_pdf = PdfFileReader("merged_2_pages.pdf")
+input_pdf = PdfFileReader("File Name")
 output = PdfFileWriter()
 output.addPage(input_pdf.getPage(0))
 with open("first_page.pdf", "wb") as output_stream:
