c86efbcfa72ac83b2f6755a8d9dc9ea66b4dc412
Rylan-Lewis <107913116+Rylan-Lewis@users.noreply.github.com>
2022-07-03 22:02:07 +0530

    Final code, added the two windows using tkinter

---
+++ Main.py
@@ -0,0 +1,57 @@
+from base64 import b64encode
+from tkinter import *
+from tkinter import font   
+from PIL import ImageTk, Image
+from tkinter.filedialog import askopenfile
+from tkinter.messagebox import showinfo
+from PyPDF2 import PdfFileMerger, PdfFileReader
+import re
+from pathlib import Path
+import os
+import subprocess
+import tabula
+from pdf2docx import Converter
+from docx2pdf import convert
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+proot= Tk()
+proot.title("Validation Window")
+proot.geometry("800x800")
+proot.maxsize(800,800)
+proot.minsize(500,500)
+px=Label(proot, text="ENTRY GATE", font=("Calibri",15, "italic"), bg="White", fg="Black", width=12, height=1, relief=SUNKEN, padx= 20, pady=10)
+px.pack()
+key1=Label(proot, text='To convert your files, Enter the Passkey:', font=("Calibri", 15))
+key1.pack()
+pa1= StringVar() 
+pa1e= Entry(proot,textvariable=pa1)
+pa1e.pack()
+
+photo= PhotoImage(file="minipp.png")
+z_label=Label(image=photo)
+z_label.pack()
+
+def check():
+    a=pa1.get()
+    pattern = r"\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@%&])[A-Za-z\d@%&]{5,10}$"
+    x = re.search(pattern,a)
+    
+    if x:
+        window_root = Tk()
+        #window specifications
+        window_root.geometry("1000x1000")
+        window_root.minsize(300,300)
+        window_root.maxsize(800,800)
+        window_root.configure(background='Grey')
+        window_root.title("FILE CONVERTER SYSTEM")
+        tx= Label(window_root,text="Convert your Files!" , font={"Arial", 20, "bold"},  background="Red", foreground="White" , width="12", height="1", relief=SUNKEN, padx=20, pady=10)
+        tx.pack()
+        window_root.mainloop()
+    else:
+        showinfo("INVALID")
+        
+
+pb=Button(proot, text='Validate', command=check)
+pb.pack()
+
+proot.mainloop()
